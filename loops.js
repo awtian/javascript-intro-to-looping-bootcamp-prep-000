a9e2faedb0271ed010c729d092c9cf106a9b5ebe
Awtian Akbar <me@awtian.com>
2017-10-30 03:50:05 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,11 @@
 function forLoop (array) {
   for (var i= 0; i < 25; i++)
   if (i == 1) {
-  return array.push `I am ${i} strange loop.`
+    array.push `I am ${i} strange loop.`
 } else {
-  return array.push `i am ${i} strange loops.`
+   array.push `i am ${i} strange loops.`
 }
-
+return array
 }
 
 var a = [];
