560aef128a7be64c490895100eee64ce64ce6e46
Awtian Akbar <me@awtian.com>
2017-10-30 03:40:03 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop (array) {
   for (var i= 1; i < 25; i++)
   if (i == 1) {
-  return `I am ${i} strange loop.`
+  return `I am strange loop.`
 } else {
   return `i am ${i} strange loops.`
 }
